594e5eeb00b978084ec7ce4ef6a72dba34eba8bc
Tyler Clark <tylerc111@gmail.com>
2024-04-09 08:25:01 -0400

    further troubleshooting attempt in install.js?

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,9 +3,9 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    const windowPrompt = windowPrompt.deferredPrompt;
+    // const windowPrompt = windowPrompt.deferredPrompt;
     // event.preventDefault();
-    windowPrompt = event;
+    window.deferredPrompt = event;
     butInstall.style.display = 'block';
 });
 
@@ -14,13 +14,13 @@ butInstall.addEventListener('click', async () => {
     const windowPrompt = window.deferredPrompt;
     if (windowPrompt) {
        windowPrompt.prompt();
-        // const choiceResult = await deferredPrompt.userChoice
-        // if (choiceResult.outcome === 'accepted') {
-        //     console.log('User accepted the installation prompt');
-        //   } else {
-        //     console.log('User dismissed the installation prompt');
-        //   }
-          windowPrompt = null;
+        const choiceResult = await deferredPrompt.userChoice
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the installation prompt');
+          } else {
+            console.log('User dismissed the installation prompt');
+          }
+          window.deferredPrompt = null;
           butInstall.style.display = 'none';
     }
 });
