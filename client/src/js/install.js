ef4c98a1ff3d340db8157a5fb34334b46eb270ec
Tyler Clark <tylerc111@gmail.com>
2024-04-09 08:50:49 -0400

    another attempt to fix the stubborn install issue

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -14,7 +14,7 @@ butInstall.addEventListener('click', async () => {
     const windowPrompt = window.deferredPrompt;
     if (windowPrompt) {
        windowPrompt.prompt();
-        const choiceResult = await window.deferredPrompt.userChoice
+        const choiceResult = await windowPrompt.userChoice
         if (choiceResult.outcome === 'accepted') {
             console.log('User accepted the installation prompt');
           } else {
