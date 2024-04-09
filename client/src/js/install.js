fb59c6ff537350c4fff94ef76c6eae1aa6f016b2
Tyler Clark <tylerc111@gmail.com>
2024-04-08 21:04:13 -0400

    another attempt to fix install.js bug

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,24 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
+const deferredPrompt = window.deferredPrompt;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    window.deferredPrompt = event;
+    deferredPrompt = event;
     butInstall.style.display = 'block';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    if (window.deferredPrompt) {
-        window.deferredPrompt.prompt();
-        const choiceResult = await window.deferredPrompt.userChoice
+    if (deferredPrompt) {
+       deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice
         if (choiceResult.outcome === 'accepted') {
             console.log('User accepted the installation prompt');
           } else {
             console.log('User dismissed the installation prompt');
           }
-          window.deferredPrompt = null;
+          deferredPrompt = null;
           butInstall.style.display = 'none';
     }
 });
